82fe375c0ebc263ebe23ee90d5ab32fd9f5b2180
Henrique Fernandez <rickfernandez03@gmail.com>
2023-05-31 22:13:43 -0300

    code updated. need to adjust register screen and implement back-end

--- app/src/main/java/br/com/fiap/greenfield/MainActivity.kt
+++ app/src/main/java/br/com/fiap/greenfield/MainActivity.kt
@@ -6,36 +6,43 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import br.com.fiap.greenfield.databinding.ActivityRegisterBinding
 import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.Response
 
-
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val seachZIP: Button = findViewById(R.id.seachZIP_button)
-        val zip: EditText = findViewById(R.id.zip_input)
-        val street: EditText = findViewById(R.id.street_input)
-        val complement: EditText = findViewById(R.id.complement_input)
-        val district: EditText = findViewById(R.id.district_input)
-        val city: EditText = findViewById(R.id.city_input)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        seachZIP.setOnClickListener {
+        val zip: TextView = binding.zip
+        val street: ConstraintLayout = binding.streetInput
+        val complement: ConstraintLayout = binding.complementInput
+        val district: ConstraintLayout = binding.districtInput
+        val city: ConstraintLayout = binding.cityInput
+        binding.searchZIPButton.setOnClickListener {
             val call = RetrofitFactory().retrofitService().getCEP(zip.text.toString())
 
-            call.enqueue(object: retrofit2.Callback<ZIP> {
+            call.enqueue(object: Callback<ZIP> {
 
                 override fun onResponse(call: Call<ZIP>, response: Response<ZIP>) {
 
                     response.body()?.let {
-                    Log.i("ZIP", it.toString())
-                    Toast.makeText(this@MainActivity, it.toString(), Toast.LENGTH_LONG).show()
-                } ?: Toast.makeText(this@MainActivity, "CEP não localizado", Toast.LENGTH_LONG).show()
-            }
+                        Log.i("ZIP", it.toString())
+                        Toast.makeText(this@MainActivity, it.toString(), Toast.LENGTH_LONG).show()
+                    } ?: Toast.makeText(this@MainActivity, "CEP não localizado", Toast.LENGTH_LONG).show()
+                }
 
                 override fun onFailure(call: Call<ZIP>?, t: Throwable?) {
                     t?.message?.let { it1 -> Log.e("Erro", it1) }
@@ -43,9 +50,5 @@ class MainActivity : AppCompatActivity() {
 
             })
         }
-
-
     }
-
-
 }
--- app/src/main/java/br/com/fiap/greenfield/RetrofitFactory.kt
+++ app/src/main/java/br/com/fiap/greenfield/RetrofitFactory.kt
@@ -2,16 +2,15 @@ package br.com.fiap.greenfield
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 class RetrofitFactory {
 
-    val URL: String = "https://viacep.com.br/ws/"
+    private val url: String = "https://viacep.com.br/ws/"
 
-    val retrifitFactory = Retrofit.Builder().baseUrl(URL).addConverterFactory(GsonConverterFactory.create()).build()
+    private val retrofitFactory = Retrofit.Builder().baseUrl(url).addConverterFactory(GsonConverterFactory.create()).build()
 
     fun retrofitService() : RetrofitService {
-        return retrifitFactory.create(RetrofitService::class.java)
+        return retrofitFactory.create(RetrofitService::class.java)
     }
 
 
